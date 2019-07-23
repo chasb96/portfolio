3ced9437892b478c2720d3f725f51c66c08c770b
Charles Bassett <chasb96@gmail.com>
2019-07-23 09:50:15 -0700

    Added ability to force a reload

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![feature(plugin)]
 #![feature(proc_macro_hygiene, decl_macro)]
+extern crate dotenv;
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
 extern crate serde_derive;
 
+use rocket::request::Form;
 use rocket::response::NamedFile;
-use rocket::{get, routes};
+use rocket::response::Redirect;
+use rocket::FromForm;
+use rocket::{get, post, routes};
 use rocket_contrib::templates::Template;
 use serde_derive::Serialize;
+use std::env;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn main() {
+    dotenv::dotenv().expect("Missing .env file");
+
     rocket::ignite()
-        .mount("/", routes![home, projects, resume, download, files])
+        .mount(
+            "/",
+            routes![home, projects, resume, download, files, force_reload, force],
+        )
         .attach(Template::fairing())
         .launch();
 }
@@ -56,4 +67,34 @@ fn download(file: PathBuf) -> Option<NamedFile> {
 #[get("/assets/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("assets/").join(file)).ok()
+}
+
+#[get("/reload")]
+fn force_reload() -> Template {
+    #[derive(Serialize)]
+    struct Context {}
+
+    let context = Context {};
+
+    Template::render("views/reload", &context)
+}
+
+#[derive(FromForm)]
+struct ForceReloadPayload {
+    password: String,
+}
+
+#[post("/reload", data = "<payload>")]
+fn force(payload: Form<ForceReloadPayload>) -> Redirect {
+    let password = match env::var("ADMIN_PASSWORD") {
+        Ok(database_url) => database_url,
+        Err(_) => return Redirect::to("/"),
+    };
+
+    if password == payload.password {
+        Command::new("git").args(vec!["pull"]).output().unwrap();
+        Command::new("restart-server.sh").output().unwrap();
+    }
+
+    Redirect::to("/")
 }
